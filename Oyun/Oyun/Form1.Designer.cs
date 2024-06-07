e96a54371f6af3ba8a7935cf8b34bdd8cdd7b0d8
beritantapanci <beritantapanci@gmail.com>
2024-06-07 15:30:09 +0300

    oyun

---
+++ Oyun/Oyun/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace Oyun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pnl_txt = new System.Windows.Forms.Panel();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pnl_txt.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Harf Ver";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(93, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Harf :";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(226, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Başla\r\n\r\n";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pnl_txt
+            // 
+            this.pnl_txt.Controls.Add(this.label15);
+            this.pnl_txt.Controls.Add(this.label14);
+            this.pnl_txt.Controls.Add(this.label13);
+            this.pnl_txt.Controls.Add(this.label12);
+            this.pnl_txt.Controls.Add(this.label11);
+            this.pnl_txt.Controls.Add(this.label10);
+            this.pnl_txt.Controls.Add(this.textBox6);
+            this.pnl_txt.Controls.Add(this.textBox5);
+            this.pnl_txt.Controls.Add(this.textBox4);
+            this.pnl_txt.Controls.Add(this.textBox3);
+            this.pnl_txt.Controls.Add(this.textBox2);
+            this.pnl_txt.Controls.Add(this.textBox1);
+            this.pnl_txt.Controls.Add(this.label8);
+            this.pnl_txt.Controls.Add(this.label7);
+            this.pnl_txt.Controls.Add(this.label6);
+            this.pnl_txt.Controls.Add(this.label5);
+            this.pnl_txt.Controls.Add(this.label4);
+            this.pnl_txt.Controls.Add(this.label3);
+            this.pnl_txt.Location = new System.Drawing.Point(12, 135);
+            this.pnl_txt.Name = "pnl_txt";
+            this.pnl_txt.Size = new System.Drawing.Size(638, 86);
+            this.pnl_txt.TabIndex = 3;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(539, 59);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(66, 16);
+            this.label15.TabIndex = 17;
+            this.label15.Text = "Mn: 5 Harf\r\n";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(434, 57);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(66, 16);
+            this.label14.TabIndex = 16;
+            this.label14.Text = "Mn: 3 Harf\r\n";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(331, 59);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(66, 16);
+            this.label13.TabIndex = 15;
+            this.label13.Text = "Mn: 3 Harf\r\n";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(225, 58);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(66, 16);
+            this.label12.TabIndex = 14;
+            this.label12.Text = "Mn: 2 Harf\r\n";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(112, 59);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(66, 16);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "Mn: 3 Harf\r\n";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(3, 59);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(66, 16);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Mn: 2 Harf";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(539, 28);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(66, 22);
+            this.textBox6.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(437, 28);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(72, 22);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(334, 29);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(76, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(225, 29);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(75, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(115, 28);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(80, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(85, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(536, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 16);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Ünlü(10)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(434, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 16);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Eşya(10)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(331, 10);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 16);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Bitki(10)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(222, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 16);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Hayvan(10)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(112, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Şehir(10)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(3, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "İsim(10)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(307, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Zaman : ";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(677, 135);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 75);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Kontrol Et\r\n";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(798, 163);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(62, 22);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Puan:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(668, 239);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pnl_txt);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnl_txt.ResumeLayout(false);
+            this.pnl_txt.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel pnl_txt;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Oyun/Oyun/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Oyun
+{
+    public partial class Form1 : Form
+    {
+        string[] harfler = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "İ", "Ç", "J", "K", "L", "M", "N", "O", "Ö", "P", "R", "S", "Ş", "T", "U", "Ü", "V", "Y", "Z" };
+     
+        List<Tuple<string, int>> puanlar = new List<Tuple<string, int>>();
+
+        int saniye = 0;
+        bool harfSecildi = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pnl_txt.Enabled = false;
+            timer1.Interval = 1000;
+           // YeniHarfSec();
+        }
+
+        private void YeniHarfSec()
+        {
+            Random rnd = new Random();
+            int sayi = rnd.Next(harfler.Length);
+            label1.Text = harfler[sayi];
+            harfSecildi = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            YeniHarfSec();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (saniye < 60)
+            {
+                if (harfSecildi)
+                {
+                    timer1.Enabled = true;
+                    timer1.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Harf seçilmedi");
+                   
+                   
+                }
+                
+            }
+            
+            else
+            {
+                MessageBox.Show("Zaman doldu! Yeniden başlamak için harf seçin.");
+            }
+
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            saniye++;
+            pnl_txt.Enabled = true;
+            label2.Text = "Zaman : " + saniye.ToString();
+            if (saniye == 60)
+            {
+                timer1.Enabled = false;
+                pnl_txt.Enabled = false;
+                label1.Text = "";
+                saniye = 0;
+                harfSecildi = false;
+                MessageBox.Show("Zaman Bitti!");
+                // Süre dolduğunda "Kontrol Et" butonuna otomatik olarak tıklanmış gibi davran
+                button3_Click(sender, e);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Zamanı sıfırla
+            saniye = 0;
+            label2.Text = "Zaman : " + saniye.ToString();
+
+            // Harf seçilmedi uyarısı ver
+            if (!harfSecildi)
+            {
+                MessageBox.Show("Harf seçilmedi");
+                return;
+            }
+
+            // Puan hesapla ve göster
+            timer1.Enabled = false;
+            pnl_txt.Enabled = false;
+
+            string harf = label1.Text.ToUpper();
+            int puan = 0;
+
+            if (textBox1.Text.ToUpper().StartsWith(harf) && textBox1.Text.Length > 0) { puan += 10; }
+            if (textBox2.Text.ToUpper().StartsWith(harf) && textBox2.Text.Length > 0) { puan += 10; }
+            if (textBox3.Text.ToUpper().StartsWith(harf) && textBox3.Text.Length > 0) { puan += 10; }
+            if (textBox4.Text.ToUpper().StartsWith(harf) && textBox4.Text.Length > 0) { puan += 10; }
+            if (textBox5.Text.ToUpper().StartsWith(harf) && textBox5.Text.Length > 0) { puan += 10; }
+            if (textBox6.Text.ToUpper().StartsWith(harf) && textBox6.Text.Length > 0) { puan += 10; }
+
+            MessageBox.Show("Puanınız: " + puan.ToString());
+            MessageBox.Show("Lütfen harf değiştirip devam ediniz");
+            label9.Text = " Toplam Puan : " + puan.ToString();
+            puanlar.Add(new Tuple<string, int>(harf, puan));
+            GuncellePuanlar();
+
+            Temizle();
+        }
+
+        private void GuncellePuanlar()
+        {
+            listBox1.Items.Clear();
+            int toplamPuan = 0;
+            foreach (var puan in puanlar)
+            {
+                listBox1.Items.Add("Harf: " + puan.Item1 + ", Puan: " + puan.Item2);
+                toplamPuan += puan.Item2;
+            }
+            label9.Text = "Toplam Puan : " + toplamPuan.ToString();
+        }
+
+
+        private void Temizle()
+        {
+            foreach (TextBox textBox in pnl_txt.Controls.OfType<TextBox>())
+            {
+                textBox.Clear();
+            }
+        }
+
+       
+
+      
+    }
+}
